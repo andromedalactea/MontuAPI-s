2aafde436e59d1b71b920cbd714f5d1119c55210
andromedalactea <bry3639@gmil.com>
2023-12-21 11:32:46 -0500

    [FEAT] inicitial files from Montu Python

---
+++ handler.py
@@ -0,0 +1,2 @@
+[InternetShortcut]
+URL=vscode-remote://wsl%2Bubuntu/home/clickgreen/MontuPython/handler.py
---
+++ time_api.py
@@ -0,0 +1,2 @@
+[InternetShortcut]
+URL=vscode-remote://wsl%2Bubuntu/home/clickgreen/MontuPython/API/time_api.py
